1661416dd4df4d6413efef7029da7c115cdffa3d
Sambo <losangeles9@gmail.com>
2016-01-12 16:20:32 +0100

    ex_06

---
+++ ex_06/ex_6.rb
@@ -0,0 +1,16 @@
+#!/usr/bin ruby
+
+l = lambda{print "hello "}
+
+# def sayTiti
+#   yield
+#   puts 'titi'
+# end
+
+# sayTiti &l
+
+# def sayTiti
+# yield("hello ")
+# puts 'titi'
+# end
+# sayTiti &l
