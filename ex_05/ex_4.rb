99575183ebe89d0381208c2d6e4192a5779491b4
Sambo <losangeles9@gmail.com>
2016-01-12 16:08:40 +0100

    ex_05

---
+++ ex_05/ex_4.rb
@@ -0,0 +1,45 @@
+#!/usr/bin ruby
+# load "ex_5.rb"
+                          # =====================================
+                          # ========= Class MyContainer =========
+                          # =====================================
+
+class MyContainer
+
+  # ========= Attributes =========
+  attr_reader :data
+
+  # ========= Constructor =========
+  def initialize()
+    @data = Array.new
+  end
+
+  # ========= Methods =========
+  def my_each
+    for i in 0...@data.length
+      yield @data[i]
+    end
+  end
+  def <<(el)
+    @data.push(el)
+    self
+  end
+  def +(el)
+    @data = @data + el.data
+    self
+  end
+  def to_s
+    @data.join(", ")
+  end
+end
+
+#
+# ctn1 = MyContainer.new
+# ctn2 = MyContainer.new
+# ctn1 << 1 << "toto" << "titi"
+# ctn2 << 42
+# ctn2 << "tata"
+# ctn1 += ctn2
+# puts ctn1
+# ctn1.my_each {|elem| puts elem}
+# ctn1.my_each &var_dump_array
